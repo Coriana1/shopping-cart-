7c15f45bff4908eb58ea20f1abccb01017b73737
Coriana Williams <corianacory@gmail.com>
2023-03-23 19:26:44 -0400

    added updated to app and catalog js

--- js/app.js
+++ js/app.js
@@ -14,20 +14,26 @@ const Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  let cartItemObj = new CartItem(product, quantity);
+  this.items.push(cartItemObj);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.cart = JSON.stringify(this.items);
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  this.items.splice(item, 1);
 };
 
 Cart.prototype.updateCounter = function() {
   // TODO: Update the cart count in the header nav with the number of items in the Cart
-}
+
+};
+
 
 const CartItem = function(product, quantity) {
   this.product = product;
@@ -64,4 +70,5 @@ function generateCatalog() {
 }
 
 // Initialize the app by creating the big list of products with images and names
-generateCatalog();
+// generateCatalog();
+
--- js/catalog.js
+++ js/catalog.js
@@ -12,7 +12,10 @@ function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in state.allProducts) {
-
+    //console.log(Product.allProducts[i].name);
+    let select = document.createElement ('option');
+    select.textContent = Product.allProducts[i].name;
+    selectElement.appendChild(select);
   }
 
 }
@@ -21,7 +24,7 @@ function populateForm() {
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
-
+  event.preventDefault();
   // TODO: Prevent the page from reloading
 
   // Do all the things ...
@@ -35,14 +38,39 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  const selectElement = document.getElementById('items');
+  pickedItem = selectElement.options[selectElement.selectedIndex].value;
   // TODO: get the quantity
+  qtyValue= document.getElementById('quantity').value;
   // TODO: using those, add one item to the Cart
+  cart.addItem(pickedItem,qtyValue);
 }
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
+  let cartCounter =JSON.parse(localStorage.cart).length;
+  //console.log(cartCounter);
+
+  let spanElement = document.getElementById('itemCount');
+  spanElement.textContent=cartCounter;
+ //document.getElementsByTagName('a').value=cartCounter;
+}
   // TODO: Get the item and quantity from the form
+  function updateCartPreview() {
+    // TODO: Get the item and quantity from the form
+    const selectElement = document.getElementById('items');
+    pickedItem = selectElement.options[selectElement.selectedIndex].value;
+    qtyValue= document.getElementById('quantity').value;
   // TODO: Add a new element to the cartContents div with that information
+  let divElement= document.getElementById('cartContents')
+  let pelement = document.createElement('p');
+  divElement.appendChild(pelement);
+  let h1element = document.createElement('h1');
+  h1element.textContent=pickedItem;
+  pelement.appendChild(h1element);
+  let h1element2 = document.createElement('h1');
+  h1element2.textContent=qtyValue;
+  pelement.appendChild(h1element2);
 }
 
 // Set up the "submit" event listener on the form.
